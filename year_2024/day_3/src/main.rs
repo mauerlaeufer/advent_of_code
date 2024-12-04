07c19f0f644af93688af9f5d619b4ec883d51b22
mauerlaeufer <hanno.kruempelmann@gmail.com>
2024-12-04 10:06:31 +0100

    Initial commit.

---
+++ year_2024/day_3/src/main.rs
@@ -0,0 +1,49 @@
+use regex::Regex;
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./input").unwrap();
+    part_one(&input);
+    part_two(&input)
+}
+
+fn regex_wrapper(string: &str, regex_pattern: &str) -> Vec<String> {
+    let pattern = Regex::new(regex_pattern).unwrap();
+    pattern
+        .find_iter(string)
+        .map(|v| v.as_str().to_owned())
+        .collect::<Vec<String>>()
+}
+
+fn part_one(input: &str) {
+    let res = regex_wrapper(input, r"mul\(\d{1,4}\,\d{1,4}\)")
+        .into_iter()
+        .map(|r| regex_wrapper(&r, r"\d{1,4}"))
+        .into_iter()
+        .map(|v| v[0].parse::<i32>().unwrap() * v[1].parse::<i32>().unwrap())
+        .sum::<i32>();
+    println!("Part 1: {res:?}");
+}
+
+fn part_two(input: &str) {
+    let res = regex_wrapper(input, r"mul\(\d{1,4}\,\d{1,4}\)|don?\'?t?\(\)");
+    let mut sum = 0;
+    let mut switch = true;
+    for val in res {
+        match val.as_str() {
+            "do()" => switch = true,
+            "don't()" => switch = false,
+            _ => {
+                if switch {
+                    sum += regex_wrapper(&val, r"\d{1,4}")
+                        .into_iter()
+                        .map(|v| v.parse::<i32>().unwrap())
+                        .product::<i32>()
+                } else {
+                    println!("blocked");
+                }
+            }
+        }
+    }
+    println!("Part 2: {sum:?}")
+}
---
+++ year_2024/day_4/src/main.rs
@@ -0,0 +1,103 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./input").unwrap();
+    part_one(&input);
+    part_two(&input);
+    println!("Hello, world!");
+}
+
+fn part_one(input: &str) {
+    let mut counter = 0;
+    let forward = "XMAS";
+    let backwards = "SAMX";
+    let forward_tup = ('X', 'M', 'A', 'S');
+    let backwards_tup = ('S', 'A', 'M', 'X');
+
+    let arr: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    // Grid is square for some reason.
+    let size = arr.len() - 1;
+
+    // top-left to bottom-right
+    for col in 0..(size - 2) {
+        let mut row = 0;
+        while row + 2 < size {
+            let tup = (
+                arr[row][col],
+                arr[row + 1][col + 1],
+                arr[row + 2][col + 2],
+                arr[row + 3][col + 3],
+            );
+            if tup == forward_tup || tup == backwards_tup {
+                counter += 1;
+            }
+            row += 1;
+        }
+    }
+
+    // top-right to bottom-left
+    for col in (3..(size + 1)).rev() {
+        let mut row = 0;
+        while row + 2 < size {
+            let tup = (
+                arr[row][col],
+                arr[row + 1][col - 1],
+                arr[row + 2][col - 2],
+                arr[row + 3][col - 3],
+            );
+            if tup == forward_tup || tup == backwards_tup {
+                counter += 1;
+            }
+            row += 1;
+        }
+    }
+
+    // Vertical
+    for col in 0..(size + 1) {
+        let mut row = 0;
+        while row + 2 < size {
+            let tup = (
+                arr[row][col],
+                arr[row + 1][col],
+                arr[row + 2][col],
+                arr[row + 3][col],
+            );
+            if tup == forward_tup || tup == backwards_tup {
+                counter += 1;
+            }
+            row += 1;
+        }
+    }
+
+    // Horizontal
+    for line in input.lines() {
+        for i in 0..(line.len() - 4) {
+            let win = &line[i..(i + 4)];
+            if win == forward || win == backwards {
+                counter += 1;
+            }
+        }
+    }
+    println!("Part One: {}", counter);
+}
+
+fn part_two(input: &str) {
+    let arr: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    // Grid is square for some reason.
+    let size = arr.len();
+    let mut counter = 0;
+
+    let forward_tup = ('M', 'A', 'S');
+    let backward_tup = ('S', 'A', 'M');
+
+    for col in 0..(size - 2) {
+        for row in 0..(size - 2) {
+            let a = (arr[col][row], arr[col + 1][row + 1], arr[col + 2][row + 2]);
+            let b = (arr[col + 2][row], arr[col + 1][row + 1], arr[col][row + 2]);
+            if (a == forward_tup || a == backward_tup) && (b == forward_tup || b == backward_tup) {
+                counter += 1;
+            }
+        }
+    }
+    println!("Part 2: {counter}");
+}
